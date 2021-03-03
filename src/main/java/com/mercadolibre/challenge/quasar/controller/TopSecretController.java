e8c0549cd2d29739a0ea27deba5652abec406fba
jaramirez85 <jaramirez85@gmail.com>
2021-03-02 19:24:48 -0500

    Swagger Documentation was added

--- src/main/java/com/mercadolibre/challenge/quasar/controller/TopSecretController.java
+++ src/main/java/com/mercadolibre/challenge/quasar/controller/TopSecretController.java
@@ -1,8 +1,12 @@
 package com.mercadolibre.challenge.quasar.controller;
 
 import com.mercadolibre.challenge.quasar.domain.TopSecretRequest;
+import com.mercadolibre.challenge.quasar.domain.TopSecretResponse;
 import com.mercadolibre.challenge.quasar.service.TopSecretService;
 import com.mercadolibre.challenge.quasar.service.exception.TopSecretException;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,8 +28,13 @@ public class TopSecretController {
         return "topsecret endpoint is ok";
     }
 
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully response", response = TopSecretResponse.class),
+            @ApiResponse(code = 404, message = "Position or message not determined", response = String.class),
+            @ApiResponse(code = 500, message = "Internal error", response = String.class)})
     @PostMapping
-    public ResponseEntity<Object> topSecret(@RequestBody TopSecretRequest topSecretRequest) {
+    public ResponseEntity<Object> topSecret(@ApiParam(value = "TopSecretRequest", required = true)  @RequestBody TopSecretRequest topSecretRequest) {
         try{
             return new ResponseEntity<>(topSecretService.processData(topSecretRequest), HttpStatus.OK);
         }catch (TopSecretException ex){
--- src/main/java/com/mercadolibre/challenge/quasar/controller/TopSecretSplitController.java
+++ src/main/java/com/mercadolibre/challenge/quasar/controller/TopSecretSplitController.java
@@ -1,7 +1,11 @@
 package com.mercadolibre.challenge.quasar.controller;
 
+import com.mercadolibre.challenge.quasar.domain.TopSecretResponse;
 import com.mercadolibre.challenge.quasar.domain.UpdateSatelliteRequest;
 import com.mercadolibre.challenge.quasar.service.TopSecretService;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +22,12 @@ public class TopSecretSplitController {
     @Autowired
     private final TopSecretService topSecretService;
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully response"),
+            @ApiResponse(code = 500, message = "Internal error", response = String.class)})
     @PostMapping("/{satellite_name}")
-    public ResponseEntity<String> updateSatellite(@PathVariable("satellite_name") String satelliteName, @RequestBody UpdateSatelliteRequest updateSatelliteRequest) {
+    public ResponseEntity<String> updateSatellite(@ApiParam(value = "Satellite name", required = true) @PathVariable("satellite_name") String satelliteName,
+                                                  @ApiParam(value = "UpdateSatelliteRequest", required = true) @RequestBody UpdateSatelliteRequest updateSatelliteRequest) {
         try {
             updateSatelliteRequest.setName(satelliteName);
             topSecretService.updateSatellite(updateSatelliteRequest);
@@ -29,7 +37,9 @@ public class TopSecretSplitController {
         }
     }
 
-
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully response", response = TopSecretResponse.class),
+            @ApiResponse(code = 500, message = "Internal error", response = String.class)})
     @GetMapping
     public ResponseEntity<? extends Object> tryProcessData() {
         try {
